2593a0848affe21a80514ccad82f6c69a80d01d0
Santi Cea <sceanav@gmail.com>
2024-11-15 10:38:07 +0100

    switch

---
+++ js ejercicio1/switch.js
@@ -0,0 +1,31 @@
+const color = 'amarillo';
+switch(color) {
+	case 'rojo':
+		console.log('Has escogido rojo');
+	break;
+	case 'azul':
+		console.log('Has escogido azul');
+	break;
+	case 'amarillo':
+        console.log('Has escogido amarillo');
+        break;
+	default:
+		console.log('No has escogido ningún color de la lista!');
+}
+
+
+const golosina = 'nubes';
+
+switch(golosina) {
+	case 'chicle':
+		console.log('Has escogido un chicle');
+	break;
+	case 'gominola':
+		console.log('Has escogido una gominola');
+	break;
+	case 'regaliz':
+		console.log('Has escogido un regaliz');
+	break;
+		default:
+        console.log('No has escogido ninguna golosina de la lista!');
+}
