da2309c9d9c02bc2b3a4a92b9dfa8e719a1b1c7b
Santi Cea <sceanav@gmail.com>
2024-11-15 13:17:09 +0100

    while

--- js ejercicio1/bucles.js
+++ js ejercicio1/bucles.js
@@ -1,7 +1,7 @@
 /* for( let i = 0; i < 100; i++){console.log(i);i++;}
 
 for( let i = 0; i < 100; i++){
-    if (i%2==0) console.log(i);} */
+    if (i%2==0) console.log(i);} 
 
 for( let i = 0; i < 20; i++)
     {    
@@ -25,5 +25,30 @@ for( let i = 0; i < 20; i++)
         }
     }
 
-        
+    let suma=0;
+    for(let i=5; i>0; i--){
+        suma=suma+i;
+
+    }
+    console.log(suma);
+    for(let i=1; i<10; i++){console.log(i+' x  7 = '+7*i);}
+
+    for(let i=1; i<10; i++){
+        for(let j=1; j<10; j++){
+            console.log(i+' x '+j+' = '+i*j);
+        }
+    }*/
+
+let i =5;
+let suma=0;
+
+while (i>0) 
+    {
+        suma=suma+i;
+        i--;
+    }        
+
+console.log(suma);
+
+
     
