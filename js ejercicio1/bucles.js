3796946be8005ecc1c754da58f8fe080b6d6b395
Santi Cea <sceanav@gmail.com>
2024-11-15 11:31:52 +0100

    bucles

---
+++ js ejercicio1/bucles.js
@@ -0,0 +1,29 @@
+/* for( let i = 0; i < 100; i++){console.log(i);i++;}
+
+for( let i = 0; i < 100; i++){
+    if (i%2==0) console.log(i);} */
+
+for( let i = 0; i < 20; i++){
+    
+    if (i%3==0 && i%5==0)  
+        {            
+        console.log('fritzbuzz');
+        }
+        else 
+        { if (i%3==0)  
+            {
+            console.log('fritz');
+            }
+            else if (i%5==0)  
+                {
+                console.log('buzz');
+            }
+                else   
+                {
+                console.log(i);
+                }
+        }
+    }
+
+        
+    
