439f0205999e86e5c5f5fb789fddc37c83390c4f
Santi Cea <sceanav@gmail.com>
2024-11-15 11:38:56 +0100

    bucles

--- js ejercicio1/bucles.js
+++ js ejercicio1/bucles.js
@@ -3,8 +3,8 @@
 for( let i = 0; i < 100; i++){
     if (i%2==0) console.log(i);} */
 
-for( let i = 0; i < 20; i++){
-    
+for( let i = 0; i < 20; i++)
+    {    
     if (i%3==0 && i%5==0)  
         {            
         console.log('fritzbuzz');
