3f1073b8865b01b675368706ddd2035698857784
Santi Cea <sceanav@gmail.com>
2024-11-19 13:09:11 +0100

    funciones flecha

--- js Funciones/funciones.js
+++ js Funciones/funciones.js
@@ -1,3 +1,5 @@
+/*
+
 function verdadero(valor){
     if (valor){
         return "verdadero";
@@ -95,7 +97,7 @@ function nand(a, b) {
     return !(a && b);  }
 
 function xor(a, b) {
-    return (a || b) && !(a && b);   }
+    return !(a && b);   }
 
 
 
@@ -103,4 +105,98 @@ function xor(a, b) {
 hoyQuieroComer("pizza");
 verdadero(true);
 console.log (calcularVelocidad(100));
-console.log (calcularArea(10, 20));
+console.log (calcularArea(10, 20)); */
+    
+
+/* FUNCIONES FLECHA */
+
+const cubo = n => n*n*n;
+console.log(cubo(3));
+
+const metroshora = velocidad => velocidad * 1000;
+console.log(metroshora(100));
+
+const area = (alto, ancho) => alto * ancho;
+console.log(area(10, 20));
+
+const areatriangulo = (base, altura) => (base * altura) / 2;
+console.log(areatriangulo(10, 20)); 
+
+const calculatePerimeter = radius => 2 * Math.PI * radius;
+console.log(calculatePerimeter(5));
+
+const calculateArea = radius => Math.PI * radius * radius;
+console.log(calculateArea(5));  
+
+/* FUNCIONES DE CADENA */
+
+const logintud = nombre => nombre.length;
+console.log(logintud("pepe"));
+
+const devuelvePrimeraLetra = nombre => nombre[0];
+console.log(devuelvePrimeraLetra("pepe"));  
+
+const devuelveUltimaLetra = nombre => nombre[nombre.length - 1];
+console.log(devuelveUltimaLetra("pepe"));
+
+const devuelveEnesimaLetra = (nombre, n) => 
+    {
+    if (n>1 && n < nombre.length+1) {
+        return nombre[n-1];
+    }
+    else return "Fuera de Rango";
+}
+console.log(devuelveEnesimaLetra("pepe", 5));
+
+const tresSiete = nombre => nombre.substring(3,7);
+console.log(tresSiete("pepe es un poco aburrido"));
+
+const toCase = nombre => nombre.toLowerCase()+'-'+nombre.toUpperCase();
+console.log(toCase("pEpe"));
+
+const shorcut = (nombre, apellido) => nombre[0] +  apellido[0]; 
+console.log(shorcut("Pepe", "Perez"));
+
+const firstChar = (nombre) => {
+    let i=0;
+    while (nombre[i]==' ') {
+        i++;
+    }
+    return nombre[i];
+}
+
+console.log(firstChar("  Santi  "));
+
+const devuelveMasLarga = (cadena1, cadena2) => {
+    if (cadena1.length > cadena2.length) {
+        return cadena1;
+    }
+    else {
+        return cadena2;
+    }
+}   
+
+console.log(devuelveMasLarga("Pepe", "Pepito"));
+
+const devuelveMasLarga2 = (cadena1, cadena2) => {   
+    if (cadena1.length > cadena2.length) {
+        return cadena1; 
+    }
+    else if (cadena2.length > cadena1.length) {
+        return cadena2;
+    }
+    else {
+        return "Son iguales";
+    }
+}
+
+const generarNombre = (a,b,c) => {
+    if (a.length <5 || b.length <5 || c.length <5) {
+        return "Error";
+    }
+    else {
+        return a[0]+a[1]+b[0]+b[1]+c[0]+c[1];
+    }   
+}
+
+console.log(generarNombre("Peppee", "Papito", "Concha"));
