9cbe383ab855e43238be410da1bba5b0b9bd5990
Santi Cea <sceanav@gmail.com>
2024-12-12 23:55:05 +0100

    con mas cartas

--- React/ProyectoCartas/src/data/cartas2.js
+++ React/ProyectoCartas/src/data/cartas2.js
@@ -1,7 +1,8 @@
+import america from "../assets/img/america.png";
+import angel from "../assets/img/angel.png";
 import antorcha from "../assets/img/antorcha.png";
 import azulmanos from "../assets/img/azulmanos.png";
 import azulrayo from "../assets/img/azulrayo.png";
-import america from "../assets/img/america.png";
 import fuegocuerpo from "../assets/img/fuegocuerpo.png";
 import lanza from "../assets/img/lanza.png";
 import lila from "../assets/img/lila.png";
@@ -9,6 +10,10 @@ import verde from "../assets/img/verde.png";
 import verdeduende from "../assets/img/verdeduende.png";
 import capa from "../assets/img/capa.png";
 import reverso from "../assets/img/reverso.png";
+import fuegomano from "../assets/img/fuegomano.png";
+import ellazul from "../assets/img/ellazul.png";
+import espuma from "../assets/img/espuma.png";
+import manosfuego from "../assets/img/manosfuego.png";
 
 const cartas = [
 	{ imagen: antorcha, reverso: reverso },
@@ -21,6 +26,11 @@ const cartas = [
 	{ imagen: verde, reverso: reverso },
 	{ imagen: verdeduende, reverso: reverso },
 	{ imagen: capa, reverso: reverso },
+	{ imagen: fuegomano, reverso: reverso },
+	{ imagen: angel, reverso: reverso },
+	{ imagen: ellazul, reverso: reverso },
+	{ imagen: espuma, reverso: reverso },
+	{ imagen: manosfuego, reverso: reverso },
 	{ imagen: antorcha, reverso: reverso },
 	{ imagen: azulmanos, reverso: reverso },
 	{ imagen: azulrayo, reverso: reverso },
@@ -31,6 +41,11 @@ const cartas = [
 	{ imagen: verde, reverso: reverso },
 	{ imagen: verdeduende, reverso: reverso },
 	{ imagen: capa, reverso: reverso },
+	{ imagen: fuegomano, reverso: reverso },
+	{ imagen: angel, reverso: reverso },
+	{ imagen: ellazul, reverso: reverso },
+	{ imagen: espuma, reverso: reverso },
+	{ imagen: manosfuego, reverso: reverso },
 ];
 
 export default cartas;
