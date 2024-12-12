91c0ed3af9926346dcb1058a21c332ff9ed2e841
Santi Cea <sceanav@gmail.com>
2024-12-12 12:12:29 +0100

    pruebas

---
+++ React/ProyectoCartas/src/components/PintaBaraja.jsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+
+const PintaBaraja = ({ mazo }) => {
+	const [obj, setObj] = useState();
+	const parejaDeKeys = [];
+
+	const handleClick = (e) => {
+		if (parejaDeKeys.length < 2) {
+			parejaDeKeys.push(e.imagen.key);
+			e.descubierta = !e.descubierta;
+		} else {
+			mazo[parejaDeKeys[0]].imagen.descubierta = false;
+			mazo[parejaDeKeys[1]].imagen.descubierta = false;
+			parejaDeKeys.pop();
+			parejaDeKeys.pop();
+		}
+		setObj(e);
+	};
+
+	return (
+		<div>
+			{mazo.map((e, index) => (
+				<img
+					key={index}
+					src={e.descubierta || e.adivinada ? e.imagen : e.reverso}
+					onClick={() => {
+						handleClick(e);
+					}}
+				/>
+			))}
+		</div>
+	);
+};
+
+export default PintaBaraja;
---
+++ React/ProyectoCartas/src/data/cartas2.js
@@ -0,0 +1,56 @@
+import antorcha from "../assets/img/antorcha.png";
+import azulmanos from "../assets/img/azulmanos.png";
+import azulrayo from "../assets/img/azulrayo.png";
+import america from "../assets/img/america.png";
+import fuegocuerpo from "../assets/img/fuegocuerpo.png";
+import lanza from "../assets/img/lanza.png";
+import lila from "../assets/img/lila.png";
+import verde from "../assets/img/verde.png";
+import verdeduende from "../assets/img/verdeduende.png";
+import capa from "../assets/img/capa.png";
+import reverso from "../assets/img/reverso.png";
+
+const cartas = [
+	{ imagen: antorcha, reverso: reverso, descubierta: false, adivinada: false },
+	{ imagen: azulmanos, reverso: reverso, descubierta: false, adivinada: false },
+	{ imagen: azulrayo, reverso: reverso, descubierta: false, adivinada: false },
+	{ imagen: america, reverso: reverso, descubierta: false, adivinada: false },
+	{
+		imagen: fuegocuerpo,
+		reverso: reverso,
+		descubierta: false,
+		adivinada: false,
+	},
+	{ imagen: lanza, reverso: reverso, descubierta: false, adivinada: false },
+	{ imagen: lila, reverso: reverso, descubierta: false, adivinada: false },
+	{ imagen: verde, reverso: reverso, descubierta: false, adivinada: false },
+	{
+		imagen: verdeduende,
+		reverso: reverso,
+		descubierta: false,
+		adivinada: false,
+	},
+	{ imagen: capa, reverso: reverso, descubierta: false, adivinada: false },
+	{ imagen: antorcha, reverso: reverso, descubierta: false, adivinada: false },
+	{ imagen: azulmanos, reverso: reverso, descubierta: false, adivinada: false},
+	{ imagen: azulrayo, reverso: reverso, descubierta: false, adivinada: false },
+	{ imagen: america, reverso: reverso, descubierta: false, adivinada: false },
+	{
+		imagen: fuegocuerpo,
+		reverso: reverso,
+		descubierta: false,
+		adivinada: false,
+	},
+	{ imagen: lanza, reverso: reverso, descubierta: false, adivinada: false },
+	{ imagen: lila, reverso: reverso, descubierta: false, adivinada: false },
+	{ imagen: verde, reverso: reverso, descubierta: false, adivinada: false },
+	{
+		imagen: verdeduende,
+		reverso: reverso,
+		descubierta: false,
+		adivinada: false,
+	},
+	{ imagen: capa, reverso: reverso, descubierta: false, adivinada: false },
+];
+
+export default cartas;
