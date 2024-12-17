7c07bbb7f882ed4892cf25cab95417f5eeff2e60
Santi Cea <sceanav@gmail.com>
2024-12-17 12:00:19 +0100

    trivial acabado

--- React/Trivial/src/App.jsx
+++ React/Trivial/src/App.jsx
@@ -1,20 +1,79 @@
 import { useState } from "react";
 
 import Questions from "./assets/data/questions";
+import { Container } from "./App/styles";
+import { Row } from "./App/styles";
+import { Imagen } from "./App/styles";
+import Popup from "reactjs-popup";
+import "reactjs-popup/dist/index.css";
+
+function shuffleArray(array) {
+	for (let i = array.length - 1; i > 0; i--) {
+		let j = Math.floor(Math.random() * (i + 1));
+		[array[i], array[j]] = [array[j], array[i]];
+	}
+	return array;
+}
 
 function App() {
-	let index = 0;
+	const [questionIndex, setQuestionIndex] = useState(0);
+	const [open, setOpen] = useState(false);
+	const q = Questions[questionIndex];
+	shuffleArray(q.answers);
+
+	console.log(q);
 	return (
-		<>
-			<p>{Questions[index].question}</p>
-			<ul>
-				{Questions[index].answers.map((resp) => (
+		<div>
+			<h2
+				style={{
+					fontSize: 30,
+					fontWeight: "bold",
+					color: "#333",
+					textAlign: "center",
+					marginBottom: "50px",
+					marginTop: "50px",
+				}}
+			>
+				{q.question}
+			</h2>
+			<Container>
+				<Row>
+					{q.answers.map((resp) => (
+						<div>
+							<img src={resp.img} alt="" />
+							<p>
+								<button
+									id={resp}
+									onClick={() => {
+										if (resp.isRight) {
+											if (Questions.length - 1 != questionIndex) {
+												setQuestionIndex(questionIndex + 1);
+											} else {
+												setQuestionIndex(0);
+												setOpen(true);
+											}
+										} else if (questionIndex != 0) {
+											setQuestionIndex(questionIndex - 1);
+										}
+									}}
+								>
+									X
+								</button>{" "}
+								{resp.txt}
+							</p>
+						</div>
+					))}
+				</Row>
+				<Imagen src={q.img} alt=""></Imagen>
+				<Popup open={open} position="center" onClose={() => setOpen(false)}>
 					<div>
-						<button id={resp}>X</button> <li>{resp.txt}</li>
+						<h2>HAS GANADO</h2>
+						<p>Felicitaciones, eres un sabio</p>
+						<button onClick={() => setOpen(false)}>Continuar</button>
 					</div>
-				))}
-			</ul>
-		</>
+				</Popup>
+			</Container>
+		</div>
 	);
 }
 
---
+++ React/Trivial/src/App/styles.js
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+	width: 700px;
+	margin: auto;
+	display: flex;
+`;
+
+export const Row = styled.div`
+	display: flex;
+	flex-direction: column;
+	margin: auto;
+`;
+
+export const Imagen = styled.img`
+	width: 40%;
+    margin: auto;
+`;
